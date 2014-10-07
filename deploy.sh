25791d68ff699f03f6f87effe306df6f089080f9
Chris Collier <czcollier@gmail.com>
2014-10-07 11:57:31 -0400

    better deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ SCRIPTNAME=`basename $0`
 SCRIPTDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 WORKDIR=`pwd`
 
-DEPLOYDIR="$HOME"
+DEPLOYDIR="$HOME"/test
 
 echo "The script you are running is named: $SCRIPTNAME,
 and is located in the directory: $SCRIPTDIR
@@ -14,4 +14,15 @@ The present working directory is $WORKDIR.
 
 cd $DEPLOYDIR
 
-rsync -av --exclude-from "$SCRIPTDIR/deploy-excludes.txt" $SCRIPTDIR/ $DEPLOYDIR
+DIRS="
+.emacs.d/
+.sbt/
+.vim/"
+
+for d in $DIRS
+do
+  echo "working in dir: $d"
+  rsync -av --delete --exclude-from "$SCRIPTDIR/deploy-excludes.txt" $SCRIPTDIR/$d $DEPLOYDIR/$d
+done
+
+rsync -av --exclude "*/"  --exclude-from "$SCRIPTDIR/deploy-excludes.txt" $SCRIPTDIR $DEPLOYDIR
